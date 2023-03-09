544861d4ae775d289da493256d306c98fe758857
anubhav singh <anubhavsingh230@gmail.com>
2023-03-10 01:14:34 +0530

    two coin flip toss percent

--- flipcoin.js
+++ flipcoin.js
@@ -1,22 +1,40 @@
 // let temp = Math.round(Math.random())
 // console.log(temp)
-let headcounter = 0;
-let tellcounter = 0;
-for (let i = 1; i < 11; i++) {
+let hhcounter = 0;
+let ttcounter = 0;
+let htcounter = 0;
+let thcounter = 0;
+const coin1h = 0,
+    coin1t = 1,
+    coin2h = 2,
+    coin2t = 3;
+for (let i = 1; i < 101; i++) {
     let temp = Math.round(Math.random())
-    console.log("result", temp);
-    if (temp == 0) {
-        headcounter++;
+    let temp2 = Math.round(Math.random() + 1 * 2)
 
+    if (temp == coin1h && temp2 == coin2h) {
+        hhcounter++;
     }
-    if (temp == 1) {
-        tellcounter++;
+    if (temp == coin1t && temp2 == coin2t) {
+        ttcounter++;
+    }
+    if (temp == coin1h && temp2 == coin2t) {
+        htcounter++;
+    }
+    if (temp == coin1t && temp2 == coin2h) {
+        thcounter++;
+    }
 
-    }
 }
-let headcounterpercentage = headcounter * 100 / 10;
-let tellcounterpercentage = tellcounter * 100 / 10;
-console.log(headcounter)
-console.log(tellcounter)
-console.log(headcounterpercentage, "%")
-console.log(tellcounterpercentage, "%")
+let hhcounterpercentage = hhcounter * 100 / 100;
+let ttcounterpercentage = ttcounter * 100 / 100;
+let htcounterpercentage = htcounter * 100 / 100;
+let thcounterpercentage = thcounter * 100 / 100;
+console.log("hhcounter", hhcounter)
+console.log("ttcounter", ttcounter)
+console.log("htcounter", htcounter)
+console.log("thcounter", thcounter)
+console.log("hhcounterpercentage%", hhcounterpercentage)
+console.log("ttcounterpercentage%", ttcounterpercentage)
+console.log("htcounterpercentage%", htcounterpercentage)
+console.log("thcounterpercentage%", thcounterpercentage)
